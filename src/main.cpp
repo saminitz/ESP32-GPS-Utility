21b0637aaf5f47ee963647781024b461665cf4fa
Samuel Nitzsche <49833818+saminitz@users.noreply.github.com>
2022-07-27 14:31:58 +0200

    sdcard functionality added all nessesary functions to interact with an connected sdcard

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,164 @@
-#include <Arduino.h>
+/*
+ * Connect the SD card to the following pins:
+ *
+ * SD Card | ESP32
+ *    D2       12
+ *    D3       13
+ *    CMD      15
+ *    VSS      GND
+ *    VDD      3.3V
+ *    CLK      14
+ *    VSS      GND
+ *    D0       2  (add 1K pull up after flashing)
+ *    D1       4
+ */
+
+#include "FS.h"
+#include "SD_MMC.h"
+
+#define ONE_BIT_MODE false
+
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels) {
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if (!root) {
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if (!root.isDirectory()) {
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while (file) {
+        if (file.isDirectory()) {
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if (levels) {
+                listDir(fs, file.name(), levels - 1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void createDir(fs::FS &fs, const char *path) {
+    if (fs.mkdir(path)) {
+        Serial.printf("Created Dir: %s\n", path);
+    } else {
+        Serial.printf("mkdir failed on dir %s\n", path);
+    }
+}
+
+void removeDir(fs::FS &fs, const char *path) {
+    if (fs.rmdir(path)) {
+        Serial.printf("Removed Dir: %s\n", path);
+    } else {
+        Serial.printf("rmdir failed on file %s\n", path);
+    }
+}
+
+void readFile(fs::FS &fs, const char *path) {
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if (!file) {
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while (file.available()) {
+        Serial.write(file.read());
+    }
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message) {
+    File file = fs.open(path, FILE_WRITE);
+    if (!file) {
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if (file.print(message)) {
+        Serial.printf("Written file: %s\n", path);
+    } else {
+        Serial.printf("Writing file failed: %s\n", path);
+    }
+}
+
+void appendFile(fs::FS &fs, const char *path, const char *message) {
+    File file = fs.open(path, FILE_APPEND);
+    if (!file) {
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if (file.print(message)) {
+        // Serial.printf("Appended to file: %s\n", path);
+    } else {
+        Serial.printf("Appending to file failed: %s\n", path);
+    }
+}
+
+void renameFile(fs::FS &fs, const char *path1, const char *path2) {
+    Serial.printf("Renaming file %s to %s\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("File renamed");
+    } else {
+        Serial.println("Rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char *path) {
+    Serial.printf("Deleting file: %s\n", path);
+    if (fs.remove(path)) {
+        Serial.println("File deleted");
+    } else {
+        Serial.println("Delete failed");
+    }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    //Setting pins to work without resistors
+    pinMode(2, INPUT_PULLUP);   
+    pinMode(4, INPUT_PULLUP);
+    pinMode(12, INPUT_PULLUP);
+    pinMode(13, INPUT_PULLUP);
+    pinMode(15, INPUT_PULLUP);
+
+    if (!SD_MMC.begin("/sdcard", ONE_BIT_MODE)) {
+        Serial.println("Card Mount Failed");
+        return;
+    }
+    uint8_t cardType = SD_MMC.cardType();
+
+    if (cardType == CARD_NONE) {
+        Serial.println("No SD_MMC card attached");
+        return;
+    }
+
+    Serial.print("SD_MMC Card Type: ");
+    if (cardType == CARD_MMC) {
+        Serial.println("MMC");
+    } else if (cardType == CARD_SD) {
+        Serial.println("SDSC");
+    } else if (cardType == CARD_SDHC) {
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+    writeFile(SD_MMC, "/gps-test.txt", "Beginning of the file\n");    
+    appendFile(SD_MMC, "/gps-test.txt", "Hello World!\n");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
