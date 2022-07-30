fd0887d4f5a1ad3cc32fb1472c974243e72da344
Samuel Nitzsche <49833818+saminitz@users.noreply.github.com>
2022-07-30 22:17:40 +0200

    outsourced more sdcard code

--- lib/FileSystem/FileSystem.cpp
+++ lib/FileSystem/FileSystem.cpp
@@ -15,6 +15,8 @@
 
 #include "FileSystem.h"
 
+#define ONE_BIT_MODE false
+
 void FileSystemSetup() {
     //Setting pins to work without resistors
     pinMode(2, INPUT_PULLUP);   
@@ -22,6 +24,28 @@ void FileSystemSetup() {
     pinMode(12, INPUT_PULLUP);
     pinMode(13, INPUT_PULLUP);
     pinMode(15, INPUT_PULLUP);
+
+    if (!SD_MMC.begin("/sdcard", ONE_BIT_MODE)) {
+        Serial.println("Card Mount Failed");
+        return;
+    }
+    uint8_t cardType = SD_MMC.cardType();
+
+    if (cardType == CARD_NONE) {
+        Serial.println("No SD_MMC card attached");
+        return;
+    }
+
+    Serial.print("SD_MMC Card Type: ");
+    if (cardType == CARD_MMC) {
+        Serial.println("MMC");
+    } else if (cardType == CARD_SD) {
+        Serial.println("SDSC");
+    } else if (cardType == CARD_SDHC) {
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
 }
 
 void listDir(fs::FS &fs, const char *dirname, uint8_t levels) {
--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,12 @@
 #include <FileSystem.h>
 
-#define ONE_BIT_MODE false
-
 void setup() {
     Serial.begin(115200);
 
     FileSystemSetup();
 
-    if (!SD_MMC.begin("/sdcard", ONE_BIT_MODE)) {
-        Serial.println("Card Mount Failed");
-        return;
-    }
-    uint8_t cardType = SD_MMC.cardType();
-
-    if (cardType == CARD_NONE) {
-        Serial.println("No SD_MMC card attached");
-        return;
-    }
-
-    Serial.print("SD_MMC Card Type: ");
-    if (cardType == CARD_MMC) {
-        Serial.println("MMC");
-    } else if (cardType == CARD_SD) {
-        Serial.println("SDSC");
-    } else if (cardType == CARD_SDHC) {
-        Serial.println("SDHC");
-    } else {
-        Serial.println("UNKNOWN");
-    }
-
     writeFile(SD_MMC, "/gps-test.txt", "Beginning of the file\n");    
     appendFile(SD_MMC, "/gps-test.txt", "Hello World!\n");
-
-    listDir(SD_MMC, "/", 0);
-    createDir(SD_MMC, "/mydir");
-    listDir(SD_MMC, "/", 0);
-    removeDir(SD_MMC, "/mydir");
-    listDir(SD_MMC, "/", 2);
-    writeFile(SD_MMC, "/hello.txt", "Hello ");
-    appendFile(SD_MMC, "/hello.txt", "World!\n");
-    readFile(SD_MMC, "/hello.txt");
-    deleteFile(SD_MMC, "/foo.txt");
-    renameFile(SD_MMC, "/hello.txt", "/foo.txt");
-    readFile(SD_MMC, "/foo.txt");
-    testFileIO(SD_MMC, "/test.txt");
-    Serial.printf("Total space: %lluMB\n", SD_MMC.totalBytes() / (1024 * 1024));
-    Serial.printf("Used space: %lluMB\n", SD_MMC.usedBytes() / (1024 * 1024));
 
     Serial.println("Done!");
 }
